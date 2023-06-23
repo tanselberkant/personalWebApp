f6ceeee51037a122bbb07fc1c7129d66f26d946d
Tansel Berkant Oflaz <45034831+tanselberkant@users.noreply.github.com>
2023-06-23 12:49:32 +0300

    FIX RepoActivities component styling bug is fixed

--- components/dashboard/RepoActivities.tsx
+++ components/dashboard/RepoActivities.tsx
@@ -12,7 +12,7 @@ const RepoActivities = ({ repoCommits }: Props) => {
       <h2 className="text-2xl text-light-textHeader dark:text-dark-textHeader font-bold my-8">
         Recent Events About This Repo
       </h2>
-      <div className="border-2 border-light-cardBorder dark:border-dark-cardBorder rounded-lg shadow-lg overflow-y-hidden overflow-x-hidden">
+      <div className="border-2 border-light-cardBorder dark:border-dark-cardBorder rounded-lg shadow-lg">
         {/* REPO OWNER */}
         <div className=" p-4">
           <div className="h-12 w-12 rounded-full flex items-center ">
@@ -32,52 +32,54 @@ const RepoActivities = ({ repoCommits }: Props) => {
             </a>
           </div>
         </div>
-        <div className="w-full h-96 overflow-y-auto overflow-x-hidden ">
-          {repoCommits.map((repo: any, index: any) => (
-            <div
-              key={index}
-              className="border-2 border-light-cardBorder dark:border-dark-cardBorder p-2"
-            >
-              {/* EVENT  */}
-              <div className="flex items-center gap-4">
-                <div className="h-4 w-4 rounded-full  bg-light-textHeader dark:bg-dark-textHeader" />
-                <div className="text-light-textDescription dark:text-dark-textDescription font-semibold text-lg">
-                  Event:
+        <div className="rounded-lg p-4 border-light-cardBorder dark:border-dark-cardBorder overflow-x-hidden h-96">
+          <ul className="w-full overflow-y-auto overflow-x-hidden list-none m-0 p-0 h-[340px] space-y-4  ">
+            {repoCommits.map((repo: any, index: any) => (
+              <li
+                key={index}
+                className="border-2 border-light-cardBorder dark:border-dark-cardBorder p-2 rounded-lg"
+              >
+                {/* EVENT  */}
+                <div className="flex items-center gap-4">
+                  <div className="h-4 w-4 rounded-full  bg-light-textHeader dark:bg-dark-textHeader" />
+                  <div className="text-light-textDescription dark:text-dark-textDescription font-semibold text-lg">
+                    Event:
+                  </div>
+                  <div className="text-gray-500 text-sm ">Push Event</div>
                 </div>
-                <div className="text-gray-500 text-sm ">Push Event</div>
-              </div>
-              {/* DATE */}
-              <div className=" text-light-smallHeader dark:text-dark-smallHeader text-sm my-1">
-                {formatDate(repo.commit.author.date)}
-              </div>
-              {/* USER  */}
-              <div className="my-2">
-                <p className="text-lg text-light-textDescription dark:text-dark-textDescription">
-                  User:
-                  <span className="text-gray-500 ml-2 text-sm">
-                    tanselberkant
-                  </span>
-                </p>
-                <p className="text-lg text-light-textDescription dark:text-dark-textDescription">
-                  Repo:
-                  <span className="text-gray-500 ml-2 text-sm">
-                    tanselberkant/personalWebApp
-                  </span>
-                </p>
-              </div>
-              {/* COMMIT  */}
-              <div className="flex my-2">
-                <ChatBubbleLeftEllipsisIcon className="w-6 h-6 text-light-smallHeader dark:text-dark-smallHeader" />
-                <div className="ml-2 text-base text-light-textDescription dark:text-dark-textDescription">
-                  Commits
+                {/* DATE */}
+                <div className=" text-light-smallHeader dark:text-dark-smallHeader text-sm my-1">
+                  {formatDate(repo.commit.author.date)}
                 </div>
-              </div>
-              {/* COMMIT MESSAGE */}
-              <div className="text-gray-500 text-sm">
-                {repo.commit.message}{' '}
-              </div>
-            </div>
-          ))}
+                {/* USER  */}
+                <div className="my-2">
+                  <p className="text-lg text-light-textDescription dark:text-dark-textDescription">
+                    User:
+                    <span className="text-gray-500 ml-2 text-sm">
+                      tanselberkant
+                    </span>
+                  </p>
+                  <p className="text-lg text-light-textDescription dark:text-dark-textDescription">
+                    Repo:
+                    <span className="text-gray-500 ml-2 text-sm">
+                      tanselberkant/personalWebApp
+                    </span>
+                  </p>
+                </div>
+                {/* COMMIT  */}
+                <div className="flex my-2">
+                  <ChatBubbleLeftEllipsisIcon className="w-6 h-6 text-light-smallHeader dark:text-dark-smallHeader" />
+                  <div className="ml-2 text-base text-light-textDescription dark:text-dark-textDescription">
+                    Commits
+                  </div>
+                </div>
+                {/* COMMIT MESSAGE */}
+                <div className="text-gray-500 text-sm">
+                  {repo.commit.message}{' '}
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
