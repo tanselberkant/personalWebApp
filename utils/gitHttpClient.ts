fb9257f335291d18573fe7a9cf7aeb7d6c96e309
Tansel Berkant Oflaz <45034831+tanselberkant@users.noreply.github.com>
2023-06-29 18:55:52 +0300

    FIX yarn.lock error

--- utils/gitHttpClient.ts
+++ utils/gitHttpClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   headers: {
-    Authorization: `${process.env.NEXT_PUBLIC_GITHUB_API_TOKEN}`,
+    // Authorization: `${process.env.NEXT_PUBLIC_GITHUB_API_TOKEN}`,
     'Access-Control-Allow-Origin': '*',
   },
   baseURL: 'https://api.github.com/',
