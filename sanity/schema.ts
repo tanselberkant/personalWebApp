0514ee55b5e8cf5677d02917e767b998ca8ba90b
Tansel Berkant Oflaz <45034831+tanselberkant@users.noreply.github.com>
2023-09-04 02:00:42 +0300

    another section is added

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -5,7 +5,8 @@ import category from './schemas/category';
 import post from './schemas/post';
 import author from './schemas/author';
 import cv from './schemas/cv';
+import project from './schemas/project';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent, cv],
+  types: [post, author, category, blockContent, cv, project],
 };
---
+++ sanity/schemas/project.ts
@@ -0,0 +1,60 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Project Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'link',
+      title: 'Project Link',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Project Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative Text',
+        },
+      ],
+    }),
+    defineField({
+      name: 'technologies',
+      title: 'Used Technologies',
+      type: 'array',
+      of: [{ type: 'string' }],
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+});
